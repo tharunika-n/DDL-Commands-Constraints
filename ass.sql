79a8c48482660d80657bf6d8c0ac989a8f5d5724
tharunika-n <tharunikanagendran43@gmail.com>
2026-09-25 23:07:23 +0530

    Add files via upload

---
+++ ass.sql
@@ -0,0 +1,17 @@
+CREATE TABLE employees (
+    employee_id INT PRIMARY KEY,
+    employee_name VARCHAR(50) NOT NULL,
+    gender ENUM('M', 'F'),
+    age INT CHECK (age >= 18),
+    hire_date DATE DEFAULT (CURRENT_DATE),
+    designation VARCHAR(100),
+    department_id INT,
+    location_id INT,
+    salary DECIMAL(10,2),
+
+    FOREIGN KEY (department_id)
+        REFERENCES departments(department_id),
+
+    FOREIGN KEY (location_id)
+        REFERENCES location(location_id)
+);
